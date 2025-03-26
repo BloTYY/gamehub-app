3e6beb31d6744714070fc9f2058435f5be72bb35
danila <lackyneon228@gmail.com>
2025-03-26 03:59:05 +0300

    Update package files

--- gamehub/vite.config.js
+++ gamehub/vite.config.js
@@ -3,18 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
+    base: '/',
     build: {
-        outDir: 'dist',
-        emptyOutDir: true,
-    },
-    server: {
-        host: '0.0.0.0',
-        port: 3000,
-        strictPort: true,
-        hmr: {
-            protocol: 'ws',
-            host: 'localhost'
-        }
-    },
-    base: './'
+        outDir: 'dist', 
+        emptyOutDir: true
+    }
 })
